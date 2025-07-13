874d48c093db842768909b9d67cf0d68877d926c
mcegg <78846869+dmcegan@users.noreply.github.com>
2025-07-13 16:00:55 +1000

    Added a buffer to eliminate jittering

--- sunfollower.ino
+++ sunfollower.ino
@@ -1,45 +1,36 @@
 #include <Servo.h>
 
-// Define servos
 Servo servoHorizontal;
 Servo servoVertical;
 
-// Define photoresistor pins
 int ldrLeft = A0;
 int ldrRight = A1;
 int ldrFront = A2;
 int ldrBack = A3;
 
-// Initial servo positions
 int angleH = 90;
 int angleV = 90;
 
 void setup() {
-  servoHorizontal.attach(9);  // Horizontal rotation
-  servoVertical.attach(10);   // Vertical tilt
+  servoHorizontal.attach(9);
+  servoVertical.attach(10);
   servoHorizontal.write(angleH);
   servoVertical.write(angleV);
   delay(500);
 }
 
 void loop() {
-  // Read LDR values
   int left = analogRead(ldrLeft);
   int right = analogRead(ldrRight);
   int front = analogRead(ldrFront);
   int back = analogRead(ldrBack);
 
-  // Compare sides to detect brightest direction
+  if (left - right > 20) angleH++;
+  else if (right - left > 20) angleH--;
 
-  // Horizontal adjustment
-  if (left > right) angleH++;
-  else if (right > left) angleH--;
+  if (front - back > 20) angleV--;
+  else if (back - front > 20) angleV++;
 
-  // Vertical adjustment
-  if (front >back) angleV--;
-  else if (back > front) angleV++;
-
-  // Move servos
   servoHorizontal.write(angleH);
   servoVertical.write(angleV);
 
